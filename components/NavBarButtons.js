382499b66a6b1dea609a909357eaad6a5aed5333
Jefferson Sofarelli <jmsofarelli@protonmail.com>
2021-09-04 12:37:58 +0100

    Implemented file upload to IPFS. Implemented connection to Terra Blockchain using Terra Station Wallet Chrome extension. Enhanced the UI.

--- components/NavBarButtons.js
+++ components/NavBarButtons.js
@@ -1,14 +1,45 @@
 import Link from 'next/link';
+import { useWallet, WalletStatus } from '@terra-money/wallet-provider';
+
+
 
 export default function NavBarButtons() {
+  const { 
+    status, network, wallets, availableConnectTypes, 
+    availableInstallTypes, connect, install, disconnect
+  } = useWallet();
+
+  let address = "";
+  if (wallets[0] && wallets[0].terraAddress) {
+    const terraAddr = wallets[0].terraAddress;
+    address = `${terraAddr.slice(0,7)}...${terraAddr.slice(terraAddr.length - 6)} (${network.name})`;
+  }
+
   return (
     <div className="flex items-center">
       <div className="flex-shrink-0">
-        <Link href="/">
-          <button type="button" className="relative inline-flex items-center mr-2 px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500">
-            <span>Connect Wallet</span>
+        {status === WalletStatus.WALLET_NOT_CONNECTED ? (
+          <button
+              className="relative inline-flex items-center mr-2 px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
+              onClick={() => connect("CHROME_EXTENSION")}
+          >
+            Connect Wallet
           </button>
-        </Link>
+        ) : (
+          <div className="">
+            { address && (
+              <span className="inline-block text-sm text-white px-4 py-2 border border-white rounded-md mr-3">
+                {address}              
+              </span>
+            )}
+            <button
+              className="relative inline-flex items-center mr-2 px-4 py-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-500 hover:bg-indigo-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-indigo-500"
+              onClick={() => disconnect()}
+            >
+              Disconnect
+            </button>
+          </div>
+        )}
       </div>
     </div>
   );
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import { useWallet, WalletStatus } from '@terra-money/wallet-provider';
 import { useState } from 'react';
+import { create } from 'ipfs-http-client';
 import api from "../lib/api";
 import NavBar from '../components/NavBar';
 
-const API_HOST = process.env.NEXT_PUBLIC_API_HOST;
-const GENERATE_FROM_FILE_PATH = process.env.NEXT_PUBLIC_GENERATE_FROM_FILE_PATH;
+// ISCC api 
+const ISCC_BASE_URL = process.env.NEXT_PUBLIC_ISCC_BASE_URL;
+const ISCC_GENERATE_FROM_FILE_API = process.env.NEXT_PUBLIC_ISCC_GENERATE_FROM_FILE_API;
+
+// IPFS api
+const IPFS_BASE_URL = process.env.NEXT_PUBLIC_IPFS_BASE_URL;
+const IPFS_API = process.env.NEXT_PUBLIC_IPFS_API;
+const ipfsClient = create(`${IPFS_BASE_URL}${IPFS_API}`);
 
 export default function Home() {
   const { 
@@ -16,6 +23,7 @@ export default function Home() {
   const [uuid, setUuid] = useState(); // should be generated as id?
   const [mediaFile, setMediaFile] = useState(null);
   const [mediaData, setMediaData] = useState(null);
+  const [mediaPath, setMediaPath] = useState("");
   const [name, setName] = useState("");
   const [isccCode, setIsccCode] = useState("");
   const [tophash, setTophash] = useState("");
@@ -23,38 +31,52 @@ export default function Home() {
 
   // UI state
   const [generatingIscc, setGeneratingIscc] = useState(false);
+  const [uploadingToIpfs, setUploadingToIpfs] = useState(false);
   const [successMsg, setSuccessMsg] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
   
   // Handle file upload
   async function onFileUpload(evt) {
-    setGeneratingIscc(true);
     const file = evt.target.files[0];
     setMediaFile(file);
     setMediaData({
       name: file.name,
-      size: file.size, // TODO format in kB or mB
+      size: file.size, // TODO format in kB or MB when rendering
     });
-    const method = 'POST';
-    const url = `${API_HOST}${GENERATE_FROM_FILE_PATH}`;
-    const contentType = 'multipart/form-data';
-    const data = { title: name };
-    const formData = new FormData();
-    formData.append("data", JSON.stringify(data));
-    formData.append("file", file);
-
+    
     try {
-      const response = await api.request({ method, url, data: formData, headers: { "Content-Type": contentType } });
+      // generate ISCC code
+      const url = `${ISCC_BASE_URL}${ISCC_GENERATE_FROM_FILE_API}`;
+      const data = { title: name };
+      const formData = new FormData();
+      formData.append("data", JSON.stringify(data));
+      formData.append("file", file);
+      setGeneratingIscc(true);
+      const response = await api.request({ method: "POST", url, data: formData, headers: { "Content-Type": "multipart/form-data" }});
       const isccData = response.data;
       setIsccCode(isccData.iscc);
       setTophash(isccData.tophash);
-      console.log('iscc', isccData.iscc);
-      console.log('tophash', isccData.tophash);
       setGeneratingIscc(false);
       setSuccessMsg('ISCC code generation successful!');
       setTimeout(() => setSuccessMsg(null), 3000);
     } catch (err) {
+      setGeneratingIscc(false);
       setErrorMsg('ISCC code generation failed!')
+      setTimeout(() => setErrorMsg(null), 3000);
+    }
+
+    try {
+      // upload file to IPFS
+      setUploadingToIpfs(true);
+      const uploadedFile = await ipfsClient.add(file)
+      const uploadedPath = `https://ipfs.infura.io/ipfs/${uploadedFile.path}`;
+      setUploadingToIpfs(false);
+      setMediaPath(uploadedPath);
+      setSuccessMsg('Upload to IPFS successful!');
+      setTimeout(() => setSuccessMsg(null), 3000);
+    } catch (err) {
+      setUploadingToIpfs(false);
+      setErrorMsg('Upload to IPFS failed!')
       setTimeout(() => setErrorMsg(null), 3000);
     }
   }
@@ -64,6 +86,7 @@ export default function Home() {
     evt.preventDefault();
     setMediaFile(null);
     setMediaData(null);
+    setMediaPath("");
     setIsccCode("");
     setTophash("");
   }
@@ -79,88 +102,101 @@ export default function Home() {
                 <div className="sm:col-span-6">
                   <label htmlFor="cover-photo" className="block text-sm font-medium text-gray-700">NFT</label>
                   <div className="mt-1 flex justify-center px-6 pt-5 pb-6 border-2 border-gray-300 border-dashed rounded-md">
-                          <div className="space-y-1 text-center">
-                            <svg
-                              className="mx-auto h-12 w-12 text-gray-400"
-                              stroke="currentColor"
-                              fill="none"
-                              viewBox="0 0 48 48"
-                              aria-hidden="true"
-                            >
-                              <path
-                                d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
+                    <div className="space-y-1 text-center">
+                      <svg
+                        className="mx-auto h-12 w-12 text-gray-400"
+                        stroke="currentColor"
+                        fill="none"
+                        viewBox="0 0 48 48"
+                        aria-hidden="true"
+                      >
+                        <path
+                          d="M28 8H12a4 4 0 00-4 4v20m32-12v8m0 0v8a4 4 0 01-4 4H12a4 4 0 01-4-4v-4m32-4l-3.172-3.172a4 4 0 00-5.656 0L28 28M8 32l9.172-9.172a4 4 0 015.656 0L28 28m0 0l4 4m4-24h8m-4-4v8m-12 4h.02"
+                          strokeWidth="2"
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                        />
+                      </svg>
+                      {mediaData && mediaData.name ? (
+                        <>
+                          <p className="text-sm text-gray-500">
+                            {mediaData.name} ({mediaData.size})
+                          </p>
+                          <div className="flex justify-center font-medium text-sm text-indigo-600">
+                            <label htmlFor="raw_pdf" className="cursor-pointer rounded-md hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                              <span>Replace</span>
+                              <input
+                                id="raw_pdf"
+                                name="raw_pdf"
+                                type="file"
+                                className="sr-only"
+                                onChange={onFileUpload}
                               />
-                            </svg>
-                            {mediaData && mediaData.name ? (
-                              <>
-                                <p className="text-sm text-gray-500">
-                                  {mediaData.name} ({mediaData.size})
-                                </p>
-                                <div className="flex justify-center font-medium text-sm text-indigo-600">
-                                  <label htmlFor="raw_pdf" className="cursor-pointer rounded-md hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                                    <span>Replace</span>
-                                    <input
-                                      id="raw_pdf"
-                                      name="raw_pdf"
-                                      type="file"
-                                      className="sr-only"
-                                      onChange={onFileUpload}
-                                    />
-                                  </label>
-                                  <span className="pl-1 hover:text-indigo-500">{" "} /{" "} <button onClick={onFileRemove}>Remove</button></span>
-                                </div>
-                              </>
-                            ) : (
-                              <>
-                                <div className="flex text-sm text-gray-600">
-                                  <label htmlFor="raw_pdf" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
-                                    <span>Upload a file</span>
-                                    <input
-                                      id="raw_pdf"
-                                      name="raw_pdf"
-                                      type="file"
-                                      className="sr-only"
-                                      onChange={onFileUpload}
-                                    />
-                                  </label>
-                                  <p className="pl-1">or drag and drop</p>
-                                </div>
-                                <p className="text-xs text-gray-500">PDF up to 30MB</p>
-                              </>
-                            )}
+                            </label>
+                            <span className="pl-1 hover:text-indigo-500">{" "} /{" "} <button onClick={onFileRemove}>Remove</button></span>
                           </div>
-                        </div>
-
-                </div>
-                <div className="sm:col-span-6">
-                  <label htmlFor="email" className="block text-sm font-medium text-gray-700">ISCC Code</label>
-                  <div className="mt-1">
-                    <input 
-                      id="name"
-                      name="name"
-                      type="text"
-                      value={isccCode}
-                      disabled
-                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                    />
+                        </>
+                      ) : (
+                        <>
+                          <div className="flex text-sm text-gray-600">
+                            <label htmlFor="raw_pdf" className="relative cursor-pointer bg-white rounded-md font-medium text-indigo-600 hover:text-indigo-500 focus-within:outline-none focus-within:ring-2 focus-within:ring-offset-2 focus-within:ring-indigo-500">
+                              <span>Upload a file</span>
+                              <input
+                                id="raw_pdf"
+                                name="raw_pdf"
+                                type="file"
+                                className="sr-only"
+                                onChange={onFileUpload}
+                              />
+                            </label>
+                            <p className="pl-1">or drag and drop</p>
+                          </div>
+                          <p className="text-xs text-gray-500">PDF up to 30MB</p>
+                        </>
+                      )}
+                    </div>
                   </div>
                 </div>
-                <div className="sm:col-span-6">
-                  <label htmlFor="email" className="block text-sm font-medium text-gray-700">Tophash</label>
+                { isccCode && (
+                  <div className="sm:col-span-3">
+                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">ISCC Code</label>
+                    <div className="mt-1">
+                      <input 
+                        id="name"
+                        name="name"
+                        type="text"
+                        value={isccCode}
+                        disabled
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                  </div> 
+                )}
+                { tophash && (
+                  <div className="sm:col-span-3">
+                    <label htmlFor="email" className="block text-sm font-medium text-gray-700">Tophash</label>
+                    <div className="mt-1">
+                      <input 
+                        id="name"
+                        name="name"
+                        type="text"
+                        value={tophash}
+                        disabled
+                        className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
+                      />
+                    </div>
+                  </div>
+                )}
+                { mediaPath && (
+                  <div className="sm:col-span-6">
+                  <label htmlFor="email" className="block text-sm font-medium text-gray-700">Media URL (IPFS)</label>
                   <div className="mt-1">
-                    <input 
-                      id="name"
-                      name="name"
-                      type="text"
-                      value={tophash}
-                      disabled
-                      className="shadow-sm focus:ring-indigo-500 focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"
-                    />
+                    <div className="border border-gray-300 rounded-md text-sm text-indigo-500 px-3 py-2">
+                      <a href={mediaPath}>{mediaPath}</a>
+                    </div>
                   </div>
                 </div>
+                )}
                 <div className="sm:col-span-6">
                   <label htmlFor="email" className="block text-sm font-medium text-gray-700">Name</label>
                   <div className="mt-1">
